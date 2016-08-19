9094d913f04cc860d2a7e35efb1332b5ff1eec88
Sergio Martins <iamsergio@gmail.com>
2016-08-19 20:56:08 +0100

    Add a window icon, mostly random for now

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 int main(int argv, char **argc)
 {
     QApplication app(argv, argc);
+    app.setWindowIcon(QIcon(":/img/snippy.png"));
     MainWindow window;
     window.show();
     return app.exec();
